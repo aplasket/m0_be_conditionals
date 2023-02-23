49b3a8d214e2f5f214ef5dda8293182e31d70456
Ashley Plasket <ashley.plasket@gmail.com>
2023-02-23 15:31:31 -0500

    Run through live for video recording

--- code_challenges/mild_challenge.rb
+++ code_challenges/mild_challenge.rb
@@ -2,8 +2,19 @@
 # Write a Ruby program that defines a variable that stores an Integer. 
 # The program should print out the String "even" if the Integer is even, and the String "odd" if the Integer is odd.
 
-nums = 81
-if nums.even? == true 
+# nums = 81
+# if nums.even? == true 
+#     p "even"
+# else
+#     p "odd"
+# end
+
+# Write a Ruby program that defines a variable that stores an Integer. 
+# The program should print out the String "even" if the Integer is even, 
+# and the String "odd" if the Integer is odd.
+
+number = 64
+if number.even? == true
     p "even"
 else
     p "odd"
