a28f525955d717b3485b676e3fcaaeb0e864314c
Ashley Plasket <ashley.plasket@gmail.com>
2023-02-23 15:51:20 -0500

    Add conditionals for car rental challenge

---
+++ code_challenges/medium_challenge.rb
@@ -0,0 +1,23 @@
+## Medium
+# Using the following variables:
+
+good_driving_record = false
+age = 21
+
+
+# Write a program that evaluates the status of the variables above, and uses logic to 
+#print one of three possible messages:
+# - If the driving record is good and user is over 25 years old, 
+#       they should get a discount on the car rental
+# - If the user either has a good record or is over 25 years old, 
+#       they should pay full price
+# - If the user is not over 25 and has a bad driving record, 
+#       they need to have someone else sign for the rental
+
+if good_driving_record == true && age > 25
+    p "You will get a discount on your car rental"
+elsif good_driving_record == true || age >= 25
+    p "you will pay full price"
+else
+    p "you need someone else to sign for the rental"
+end
