a4115cb958a139331d116c77ebc07b7dc0a007df
Ashley Plasket <ashley.plasket@gmail.com>
2023-02-23 15:17:16 -0500

    Add conditionals to determine if the integer is even or odd

---
+++ code_challenges/mild_challenge.rb
@@ -0,0 +1,10 @@
+# Mild challenge:
+# Write a Ruby program that defines a variable that stores an Integer. 
+# The program should print out the String "even" if the Integer is even, and the String "odd" if the Integer is odd.
+
+nums = 81
+if nums.even? == true 
+    p "even"
+else
+    p "odd"
+end
